d78fb2a74a2e34d6b9f48beb6f20448b9af164a5
Sanamdeep Kajla <sanamdeep.k@codebrewinnovations.com>
2023-12-28 12:54:34 +0530

    code filteration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, embeddedWallet, metamaskWallet, smartWallet } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Goerli } from "@thirdweb-dev/chains";
 import "../styles/globals.css";
-import { ACCOUNT_FACTORY_ADDRESS } from "../constants/addresses";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const smartWalletConfig = {
-    factoryAddress: ACCOUNT_FACTORY_ADDRESS,
-    gasless: true,
-  }
 
   return (
     <ThirdwebProvider
